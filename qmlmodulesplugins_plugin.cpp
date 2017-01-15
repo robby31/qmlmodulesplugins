0f6f0957cc37e19a132d82ebaeb31c35cd1aca59
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2017-01-15 09:06:39 +0100

    new SqlMapper for Chart

--- qmlmodulesplugins_plugin.cpp
+++ qmlmodulesplugins_plugin.cpp
@@ -3,12 +3,15 @@
 
 #include <qqml.h>
 #include "GraphisItem/progresscircle.h"
+#include "Models/sqllistmodel.h"
 
 void QmlmodulespluginsPlugin::registerTypes(const char *uri)
 {
     // @uri MyComponents
     qmlRegisterType<MyQmlModules>(uri, 1, 0, "MyQmlModules");
     qmlRegisterType<ProgressCircle>(uri, 1, 0, "ProgressCircle");
+    qmlRegisterType<ListModel>(uri, 1, 0, "CListModel");
+    qmlRegisterType<SqlListModel>(uri, 1, 0, "SqlListModel");
 }
 
 
