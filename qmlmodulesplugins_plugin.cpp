79a61c8f6ab46c426b29abe18fbf58e1b0e61762
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2019-01-05 10:49:27 +0100

    add SqlTableModel for qml

--- qmlmodulesplugins_plugin.cpp
+++ qmlmodulesplugins_plugin.cpp
@@ -5,6 +5,7 @@
 #include "GraphisItem/progresscircle.h"
 #include "Models/sqllistmodel.h"
 #include "SqlModel/sqlquerymodel.h"
+#include "SqlModel/sqltablemodel.h"
 #include "SqlModel/myvxymodelmapper.h"
 
 void QmlmodulespluginsPlugin::registerTypes(const char *uri)
@@ -15,6 +16,7 @@ void QmlmodulespluginsPlugin::registerTypes(const char *uri)
     qmlRegisterType<ListModel>(uri, 1, 0, "CListModel");
     qmlRegisterType<SqlListModel>(uri, 1, 0, "SqlListModel");
     qmlRegisterType<SqlQueryModel>(uri, 1, 0, "SqlQueryModel");
+    qmlRegisterType<SqlTableModel>(uri, 1, 0, "SqlTableModel");
     qmlRegisterType<MyVXYModelMapper>(uri, 1, 0, "MyVXYModelMapper");
 }
 
